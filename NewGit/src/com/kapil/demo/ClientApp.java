cc3fe65ba4ed7ad5dcc47e3d3fd748bf9ea140fa
xyz <xyz@gmail>
2020-11-19 18:27:54 +0530

    2nd Commit

--- NewGit/src/com/kapil/demo/ClientApp.java
+++ NewGit/src/com/kapil/demo/ClientApp.java
@@ -3,7 +3,7 @@ package com.kapil.demo;
 public class ClientApp {
 public static void main(String[] args) {
 	Person p = new Person("kkk",101,33333);
-	System.out.println(p);
+	System.out.print(p);
 }
 
 }
